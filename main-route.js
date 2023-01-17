a57177ccf82f628dc3c802e75131b7e9cb2e386d
Alexander Martinez <Alexander-MD@outlook.com>
2023-01-17 02:12:15 -0500

    updating the home page

--- main-route.js
+++ main-route.js
@@ -38,4 +38,3 @@ app.listen(PORT, () => {
 })
 
 
-
--- views/Homepage.jsx
+++ views/Homepage.jsx
@@ -13,6 +13,7 @@ function HomePage() {
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href='/Home-Page/HomePage.css' />
       </head>
       <body>
 
@@ -42,7 +43,9 @@ function HomePage() {
         </div>  
       </nav>
 {/* ---------------------------------------------------------------------------------- */}
-      <h1>Hello world</h1>
+      <a>
+        <h1>Hello world</h1>
+      </a>
       </body>
     </html>
 
